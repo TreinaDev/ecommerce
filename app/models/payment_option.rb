3bb99e59ce45c8cb0c7a72a51466ccd0f18ce264
Mirian Iosimura <mirian.iosimura@gmail.com>
2020-02-10 21:59:28 -0300

    WIP: terminar teste unitario de erro 500

--- app/models/payment_option.rb
+++ app/models/payment_option.rb
@@ -8,6 +8,20 @@ class PaymentOption
   end
 
   def self.all(_order_value)
-    []
+    response = Faraday.get('localhost:4000/api/v1/payment_options?order_value=100')
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    if json.any?
+      result = []
+
+      json.each do |j|
+        po = PaymentOption.new(j[:name], j[:installments].to_i, j[:installments_value].to_d)
+        result << po
+      end
+
+      result
+    else
+      
+    end
   end
 end
---
+++ spec/models/payment_option_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe PaymentOption do
+  describe ".all" do
+    it "should get payment option through API" do
+      response = double("response", status: 200, body: '[{"name" : "Cartão de Crédito", "installments" : "12", "installments_value" : "10"},
+        {"name" : "Boleto Bancário", "installments" : "1", "installments_value" : "90"}]')
+
+      allow(Faraday).to receive(:get).with("localhost:4000/api/v1/payment_options?order_value=100").and_return(response)
+
+      result = PaymentOption.all(100)
+
+      expect(result.length).to eq(2)
+      expect(result[0].name).to eq("Cartão de Crédito")
+      expect(result[0].installments).to eq(12)
+      expect(result[0].installments_value).to eq(10)
+      expect(result[1].name).to eq("Boleto Bancário")
+      expect(result[1].installments).to eq(1)
+      expect(result[1].installments_value).to eq(90)
+    end
+
+    it "should return empty array if API return error" do
+      response = double("response", status: 500, body: '[{"error" : "API indisponivel"}]')
+
+      allow(Faraday).to receive(:get).with("localhost:4000/api/v1/payment_options?order_value=100").and_return(response)
+
+      result = PaymentOption.all(100)
+
+      expect(result.length).to eq(0)
+    end
+  end
+end
