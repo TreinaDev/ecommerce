34cb8383c9d9d7a954f7ac807f6b7de0215cff6a
Andre Cesar Gomes <andre270492@gmail.com>
2020-02-15 19:57:52 -0300

    Cliente visualiza items do carrinho
    - Atualiza o preço total ao adicionar item
    
    Co-authored-by: Guilherme Sposito <developersposito@gmail.com>

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product_kit
+
+  def update_total_value
+    order.update(order_value: order.product_kits.sum(&:price))
+  end
 end
