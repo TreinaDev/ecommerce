8173e9d6f706ea197f8b3eefb14aad7f8eb0bb4b
IcaroRSiqueira <icarorob@hotmail.com>
2020-02-18 13:14:08 -0300

    Metodo de calculo de frete funcionando

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,13 @@
 class CartsController < ApplicationController
-  
   def show
     @carrier_options = CarrierOption.all
     @order = current_client.orders.find_by(status: :pending)
     @payments_options = PaymentOption.all(@order.order_value) if @order
     total_vol = CarrierPriceCalculator.new(@carrier_options, @order).total_vol
-    total_weight = CarrierPriceCalculator.new(@carrier_options, @order).total_weight
-    @carrier_attributes = CarrierPriceCalculator.new(@carrier_options, @order).calculate(total_vol, total_weight)
+    total_weight = CarrierPriceCalculator.new(@carrier_options,
+                                              @order).total_weight
+    @shipping = CarrierPriceCalculator.new(@carrier_options,
+                                           @order).calculate(total_vol,
+                                                             total_weight)
   end
 end
--- app/services/carrier_price_calculator.rb
+++ app/services/carrier_price_calculator.rb
@@ -6,52 +6,51 @@ class CarrierPriceCalculator
     @order = order
   end
 
-  def self
-  end
-
   def total_vol
-    unless order.product_kits.nil?
-      volumes = []
-      order.product_kits.all.each do |kit|
-        volumes << (kit.width/1000) * (kit.height/1000) * (kit.thickness/1000)
-      end
-      volumes.sum
+    return if order.nil?
+
+    vols = []
+    order.product_kits.all.each do |kit|
+      vols << (kit.width / 1000) * (kit.height / 1000) * (kit.thickness / 1000)
     end
+    vols.sum
   end
 
   def total_weight
-    unless order.product_kits.nil?
-      weights = []
-      order.product_kits.all.each do |kit|
-        weights << kit.weight
-      end
-      weights.sum
+    return if order.nil?
+
+    weights = []
+    order.product_kits.all.each do |kit|
+      weights << kit.weight
     end
+    weights.sum
   end
 
-  def calculate(total_vol, total_weight)
+  def calculate(total_vol, _total_weight)
+    avaiable_carrier_options = []
+    carrier_name = []
     carrier_attributes = nil
     carrier_options.map do |c_o|
       return unless (c_o.min_vol..c_o.max_vol).include?(total_vol)
-      carrier_attributes = choose_carrier(c_o)
+
+      carrier_attributes = choose_carrier(c_o, avaiable_carrier_options,
+                                          carrier_name)
     end
     carrier_attributes
   end
 
-  def choose_carrier(c_o)
-    avaiable_carrier_options = []
-    carrier_name = []
+  def choose_carrier(c_o, avaiable_carrier_options, carrier_name)
     if avaiable_carrier_options.count == 1
       if avaiable_carrier_options[0] < (total_weight * c_o.price_kg)
         avaiable_carrier_options[0]
       else
-        avaiable_carrier_options[0] == (total_weight * c_o.price_kg)
+        avaiable_carrier_options[0] = (total_weight * c_o.price_kg)
         carrier_name << c_o.carrier.name
       end
     else
       avaiable_carrier_options << (total_weight * c_o.price_kg)
       carrier_name << c_o.carrier.name
     end
-    result = {name: (carrier_name.join), price: (avaiable_carrier_options.join)}
+    { name: carrier_name.join, price: avaiable_carrier_options.join }
   end
 end
